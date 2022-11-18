67c4f0ed22212bbac77bff18bef3f954e440e7fe
SasunHakobyan <sasunhakobyan2002@gmail.com>
2022-11-18 18:45:17 +0400

    useState for drivers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import DriverList from "./components/DriverList/DriverList";
+import driver from "./components/Driver/Driver";
 
 function App() {
+
+    const initialDriversState = {
+        drivers: [],
+        dataIsLoaded: false
+    }
+
+    const [driversState, setDrivers] = useState(initialDriversState);
+
+    fetch("http://localhost:5500/getDrivers")
+        .then(res => res.json())
+        .then(res => {
+            const drivers = Array.from(res);
+            const newDriversState = Object.assign({}, driversState);
+
+            newDriversState.drivers = drivers;
+            newDriversState.dataIsLoaded = true;
+
+            // setDrivers(newDriversState);
+            console.log(driversState)
+
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
+    console.log(driversState);
+
+    if (!driversState.dataIsLoaded) {
+        return <div>Please wait</div>
+    }
+
     return (
         <BrowserRouter>
             <div className="App">
                 <Routes>
-                    <Route path='/drivers' element={<DriverList />} />
+                    <Route path='/drivers' element={<DriverList drivers={driversState.drivers} />} />
                 </Routes>
             </div>
         </BrowserRouter>
--- frontend/src/components/DriverList/DriverList.jsx
+++ frontend/src/components/DriverList/DriverList.jsx
@@ -1,21 +1,9 @@
 import Driver from "../Driver/Driver";
 import classes from './DriverList.module.css';
 
-const DriverList = (pops) => {
-    let driver1 = {
-        id: 1,
-        userName: 'Armen',
-        tariff: 'econom'
-    };
-
-    let driver2 = {
-        id: 2,
-        userName: 'Jack',
-        tariff: 'comfort'
-    }
-
-    let drivers = [driver1, driver2];
-    let driversElements = drivers.map(driver => {
+const DriverList = (props) => {
+    console.log(props)
+    let driversElements = props.drivers.map(driver => {
         return <Driver driver={driver}/>
     })
 
@@ -23,12 +11,16 @@ const DriverList = (pops) => {
         <div className={classes.driversListContainer}>
             <h3>Drivers</h3>
             <table className={classes.driversData}>
-                <tr>
-                    <th>ID</th>
-                    <th>Username</th>
-                    <th>Tariff</th>
-                </tr>
-                {driversElements}
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Username</th>
+                        <th>Tariff</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {driversElements}
+                </tbody>
             </table>
         </div>
     );
